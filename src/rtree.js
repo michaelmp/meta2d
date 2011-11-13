8802d7a9e7ad04b19b620ba371b6edb0baba3813
Michael Morris-Pearce <mikemp@mit.edu>
2011-11-13 03:27:17 -0500

    Added copy/lic statement in source files.

--- src/rtree.js
+++ src/rtree.js
@@ -1,5 +1,26 @@
-// provides:
-//  meta2d.RTree -- class
+/** rtree.js
+ *  Copyright (c) 2011 Michael Morris-Pearce <mikemp@mit.edu>
+ * 
+ *      This file is part of Meta2D.
+ *
+ *      Meta2D is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      Meta2D is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with Meta2D.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Meta2D is hosted at <https://gitorious.org/meta2d/>. Please check there for
+ *  up-to-date code, examples, documentation, and other information.
+ *----------------------------------------------------------------------------*/
+/** jslint vars: true, white: true, indent: 2, maxlen: 80, imperfection: true */
+
 (function() {
   'use strict';
   var root = this;
